859b92049473cf48dfb6149b0a95a0d69c33ecb9
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2023-09-22 21:33:47 +0700

    Initial commit

---
+++ scripts/executable_shuffle_wallpaper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wallpaper=$(find ~/Wallpapers/ -name "*.jpg" -o -name "*.png" | shuf -n 1)
+
+# Make the wallpaper persistent after reboot
+sed -E -i "s|SET_BG=.*|SET_BG=\"$wallpaper\"|g" ~/.config/HyprV/hyprv.conf
+
+swww img --transition-type wipe $wallpaper --transition-fps 144
+wal -c
+wal -q -i $wallpaper
+pkill -USR1 kitty
+pkill waybar
+waybar &
+waybar --config ~/.config/HyprV/waybar/conf/config-bottom.jsonc
