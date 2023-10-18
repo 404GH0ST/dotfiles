b685a843fd1f4ba5298f20889cb8778d4c957f91
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2023-10-18 22:32:57 +0700

    Disable bottom waybar by default

--- scripts/executable_shuffle_wallpaper.sh
+++ scripts/executable_shuffle_wallpaper.sh
@@ -5,11 +5,17 @@ wallpaper=$(find ~/Wallpapers/ -name "*.jpg" -o -name "*.png" | shuf -n 1)
 # Make the wallpaper persistent after reboot
 sed -E -i "s|SET_BG=.*|SET_BG=\"$wallpaper\"|g" ~/.config/HyprV/hyprv.conf
 
+cp -f $wallpaper ~/.config/HyprV/backgrounds/background-dark.jpg
+sudo cp -f $wallpaper /usr/share/sddm/themes/sdt/wallpaper.jpg
+
 swww img --transition-type wipe $wallpaper --transition-fps 144
+sleep 2
 wal -c
 wal -q -i $wallpaper
 pkill -USR1 kitty
 pkill -USR2 cava
 pkill waybar
-waybar &
-waybar --config ~/.config/HyprV/waybar/conf/config-bottom.jsonc
+#waybar &
+
+# Bottom waybar disabled for now
+# waybar --config ~/.config/HyprV/waybar/conf/config-bottom.jsonc
