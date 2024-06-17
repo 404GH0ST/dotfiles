8096185a3674c70f7f8cb1f9d9d74577ba767bc8
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-06-17 14:39:16 +0700

    feat: handle nvidia card case

--- scripts/configure_nvidia.sh
+++ scripts/configure_nvidia.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo pacman -S nvidia nvidia-utils libva-nvidia-driver
 sudo sed -Ei 's/(GRUB_CMDLINE_LINUX_DEFAULT="[^"]*)"/\1 nvidia_drm.modeset=1"/' /etc/default/grub
 sudo sed -Ei 's/(MODULES=\([^\)]*)/\1nvidia nvidia_modeset nvidia_uvm nvidia_drm/' /etc/mkinitcpio.conf
 sudo systemctl enable nvidia-suspend.service
