b728cc2198ce65fa2df1ba0a237e8eeaa034c9e9
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-06-18 15:08:00 +0700

    feat: add notification for brightness

--- home/.local/share/bin/brightnesscontrol.sh
+++ home/.local/share/bin/brightnesscontrol.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+send_notify() {
+	dunstify -h int:value:"$((($(cat /sys/class/backlight/*/brightness) * 100) / $(cat /sys/class/backlight/*/max_brightness)))" -i ~/.config/dunst/assets/brightness.svg -t 500 -r 2593 "Brightness: $((($(cat /sys/class/backlight/*/brightness) * 100) / $(cat /sys/class/backlight/*/max_brightness)))%"
+}
 case "$1" in
 i)
-	brightnessctl s +10
+	brightnessctl s +10 && send_notify
 	;;
 d)
-	brightnessctl s 10-
+	brightnessctl s 10- && send_notify
 	;;
 esac
