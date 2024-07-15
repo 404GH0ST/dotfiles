e34dbd5de21f123788dcf3d76fd12823bd1cba48
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-07-15 12:59:26 +0700

    feat: adjust hyprland.conf and refactor install script

--- scripts/configure_nvidia.sh
+++ scripts/configure_nvidia.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+vars=$(dirname "$0")
+source "${vars}"/vars.sh
+
 sudo pacman -S nvidia nvidia-utils libva-nvidia-driver --noconfirm
 sudo sed -Ei 's/(GRUB_CMDLINE_LINUX_DEFAULT="[^"]*)"/\1 nvidia_drm.modeset=1"/' /etc/default/grub
 sudo sed -Ei 's/(MODULES=\([^\)]*)/\1nvidia nvidia_modeset nvidia_uvm nvidia_drm/' /etc/mkinitcpio.conf
@@ -10,3 +13,6 @@ sudo sed -Ei 's/(GRUB_CMDLINE_LINUX_DEFAULT="[^"]*)"/\1 nvidia.NVreg_PreserveVid
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo mkinitcpio -P
+
+cp $repo_dir/scripts/conf/nvidia.conf $config_path/hypr/conf/nvidia.conf
+echo "source = ~/.config/hypr/conf/nvidia.conf" >>$config_path/hypr/hyprland.conf
--- scripts/vars.sh
+++ scripts/vars.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 config_path=$HOME/.config
-repo_dir=$HOME/bspwm-dotfiles
+repo_dir=$HOME/dotfiles
