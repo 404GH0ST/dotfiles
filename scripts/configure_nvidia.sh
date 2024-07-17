e95538a4df7b7c9a6a4a27196f8c79b7853a8324
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-07-17 14:53:43 +0700

    chore: Add xdg-desktop-portal-gtk for missing fileChooser

--- scripts/configure_nvidia.sh
+++ scripts/configure_nvidia.sh
@@ -4,12 +4,12 @@ vars=$(dirname "$0")
 source "${vars}"/vars.sh
 
 sudo pacman -S nvidia nvidia-utils libva-nvidia-driver --noconfirm
-sudo sed -Ei 's/(GRUB_CMDLINE_LINUX_DEFAULT="[^"]*)"/\1 nvidia_drm.modeset=1"/' /etc/default/grub
+sudo cp $repo_dir/scripts/conf/nvidia_modprobe.conf /etc/modprobe.d/nvidia.conf
 sudo sed -Ei 's/(MODULES=\([^\)]*)/\1nvidia nvidia_modeset nvidia_uvm nvidia_drm/' /etc/mkinitcpio.conf
 sudo systemctl enable nvidia-suspend.service
 sudo systemctl enable nvidia-hibernate.service
 sudo systemctl enable nvidia-resume.service
-sudo sed -Ei 's/(GRUB_CMDLINE_LINUX_DEFAULT="[^"]*)"/\1 nvidia.NVreg_PreserveVideoMemoryAllocations=1`"/' /etc/default/grub
+sudo sed -Ei 's/(GRUB_CMDLINE_LINUX_DEFAULT="[^"]*)"/\1 nvidia.NVreg_PreserveVideoMemoryAllocations=1"/' /etc/default/grub
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo mkinitcpio -P
